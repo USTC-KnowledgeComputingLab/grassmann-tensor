fcbab58061da0718ee089708f576d375c54e3d33
Gausshj <kylin_0@qq.com>
2026-01-06 19:28:40 +0800

    feat(tensor): add support for sqrt and norm
    - Add support for sqrt and norm
    - Add some test cases for sqrt and norm

---
+++ tests/utility_test.py
@@ -0,0 +1,117 @@
+import torch
+import pytest
+import itertools
+
+from grassmann_tensor import GrassmannTensor, NamedGrassmannTensor
+
+
+def generate_filled_data(
+    edges: tuple[tuple[int, int], ...], data: torch.Tensor | None = None
+) -> torch.Tensor:
+    shape = tuple(even + odd for even, odd in edges)
+
+    if data is None:
+        tensor = torch.zeros(shape)
+        filled = torch.arange(filled_count(edges))
+    else:
+        assert data is not None
+        filled = data.reshape(-1)
+        tensor = torch.zeros(shape, dtype=data.dtype, device=data.device)
+    i = 0
+    ranges = [range(s) for s in shape]
+
+    for idx in itertools.product(*ranges):
+        total_parity = 0
+        for k, (even, _) in enumerate(edges):
+            total_parity ^= 1 if idx[k] >= even else 0
+        if total_parity == 0:
+            tensor[idx] = filled[i]
+            i += 1
+    return tensor
+
+
+def filled_count(edges: tuple[tuple[int, int], ...]) -> int:
+    total = 1
+    diff = 1
+    for even, odd in edges:
+        total *= even + odd
+        diff *= even - odd
+    return (total + diff) // 2
+
+
+@pytest.mark.parametrize(
+    "edges",
+    [
+        ((1, 1),),
+        ((2, 2),),
+        ((2, 2), (2, 2)),
+        ((2, 2), (2, 2), (2, 2)),
+        ((2, 2), (2, 2), (2, 2), (2, 2)),
+    ],
+)
+def test_norm(edges: tuple[tuple[int, int], ...]) -> None:
+    arrow = tuple([False] * len(edges))
+    filled = filled_count(edges)
+    half = filled // 2
+    if filled % 2 == 0:
+        data = torch.arange(-half, half, dtype=torch.float64)
+    else:
+        data = torch.arange(-half, half + 1, dtype=torch.float64)
+    tensor_data = generate_filled_data(edges, data=data)
+    max_val = tensor_data.abs().max().item()
+    min_val = tensor_data.abs().min().item()
+
+    tensor = GrassmannTensor(arrow, edges, tensor_data)
+    assert tensor.norm(p=torch.inf) == max_val
+    assert tensor.norm(p=-torch.inf) == min_val
+    assert tensor.norm(p=0) == filled - 1
+    assert tensor.norm(p=2) == torch.linalg.vector_norm(data, ord=2)
+
+
+@pytest.mark.parametrize(
+    "edges",
+    [
+        ((1, 1),),
+        ((2, 2),),
+        ((2, 2), (2, 2)),
+        ((2, 2), (2, 2), (2, 2)),
+        ((2, 2), (2, 2), (2, 2), (2, 2)),
+    ],
+)
+def test_named_norm(edges: tuple[tuple[int, int], ...]) -> None:
+    names = tuple(chr(96 + i) for i in range(len(edges)))
+    arrow = tuple([False] * len(edges))
+    filled = filled_count(edges)
+    half = filled // 2
+    if filled % 2 == 0:
+        data = torch.arange(-half, half, dtype=torch.float64)
+    else:
+        data = torch.arange(-half, half + 1, dtype=torch.float64)
+    tensor_data = generate_filled_data(edges, data=data)
+    max_val = tensor_data.abs().max().item()
+    min_val = tensor_data.abs().min().item()
+
+    tensor = NamedGrassmannTensor(names, arrow, edges, tensor_data)
+    assert tensor.norm(p=torch.inf) == max_val
+    assert tensor.norm(p=-torch.inf) == min_val
+    assert tensor.norm(p=0) == filled - 1
+    assert tensor.norm(p=2) == torch.linalg.vector_norm(data, ord=2)
+
+
+def test_sqrt() -> None:
+    tensor = GrassmannTensor((False, False), ((1, 1), (1, 1)), torch.Tensor([[-4, 9], [0, -1]]))
+    assert torch.allclose(tensor.sqrt().tensor, torch.Tensor(([[2, 3], [0, 1]])))
+
+
+def test_named_sqrt() -> None:
+    tensor = NamedGrassmannTensor(
+        ("a", "b"), (False, False), ((1, 1), (1, 1)), torch.Tensor([[-4, 9], [0, -1]])
+    )
+    assert torch.allclose(tensor.sqrt().tensor, torch.Tensor(([[2, 3], [0, 1]])))
+
+
+def test_rank() -> None:
+    tensor = NamedGrassmannTensor(
+        ("a", "b"), (False, False), ((1, 1), (1, 1)), torch.Tensor([[-4, 9], [0, -1]])
+    )
+    assert tensor.rank() == 2
